2fd4adaa4efa52ab669e54d35dd9e94854429b63
Mads-SJ <mads.soenderstrup@gmail.com>
2023-02-23 10:03:54 +0100

    ordination implemented

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,25 @@
 package ordination;
 
-public class DagligFast {
+import java.time.LocalDate;
+
+public class DagligFast extends Ordination {
+    public DagligFast(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
     // TODO
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
 }
--- src/ordination/Ordination.java
+++ src/ordination/Ordination.java
@@ -6,9 +6,12 @@ import java.time.temporal.ChronoUnit;
 public abstract class Ordination {
     private LocalDate startDen;
     private LocalDate slutDen;
+    private Laegemiddel laegemiddel;
 
-    // TODO Link til Laegemiddel
-    // TODO constructor (med specifikation)
+    public Ordination(LocalDate startDen, LocalDate slutDen) {
+        this.startDen = startDen;
+        this.slutDen = slutDen;
+    }
 
     public LocalDate getStartDen() {
         return startDen;
@@ -24,6 +27,16 @@ public abstract class Ordination {
      */
     public int antalDage() {
         return (int) ChronoUnit.DAYS.between(startDen, slutDen) + 1;
+    }
+
+    public Laegemiddel getLaegemiddel() {
+        return laegemiddel;
+    }
+
+    public void setLaegemiddel(Laegemiddel laegemiddel) {
+        if (this.laegemiddel != laegemiddel) {
+            this.laegemiddel = laegemiddel;
+        }
     }
 
     @Override
