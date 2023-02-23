018f588ac98d8617c37be94cf10367e0a4e5d30f
Mads-SJ <mads.soenderstrup@gmail.com>
2023-02-23 10:49:34 +0100

    dagligFast implemented

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,26 +1,39 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DagligFast extends Ordination {
     private Dosis[] doses = new Dosis[4];
-    public DagligFast(LocalDate startDen, LocalDate slutDen) {
+    public DagligFast(LocalDate startDen, LocalDate slutDen, double morgenAntal, double middagAntal,
+                      double aftenAntal, double natAntal) {
         super(startDen, slutDen);
+        doses[0] = new Dosis(LocalTime.of(8, 0), morgenAntal);
+        doses[1] = new Dosis(LocalTime.of(12, 0), middagAntal);
+        doses[2] = new Dosis(LocalTime.of(18, 0), aftenAntal);
+        doses[3] = new Dosis(LocalTime.of(0, 0), natAntal);
     }
-    // TODO
+
+    public Dosis[] getDoser() {
+        return doses.clone();
+    }
 
     @Override
     public double samletDosis() {
-        return 0;
+        int samletDosis = 0;
+        for (Dosis d : doses) {
+            samletDosis += d.getAntal();
+        }
+        return samletDosis;
     }
 
     @Override
     public double doegnDosis() {
-        return 0;
+        return samletDosis() / 4;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "DagligFast";
     }
 }
