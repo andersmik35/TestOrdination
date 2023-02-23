797b6bbc5c401d5824ef868e6c87aa29c5eae1ef
burgerflipper01 <ande077h@gmail.com>
2023-02-23 11:52:35 +0100

    Changes to Controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,31 +4,27 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-import ordination.DagligFast;
-import ordination.DagligSkaev;
-import ordination.Laegemiddel;
-import ordination.PN;
-import ordination.Patient;
+import ordination.*;
 import storage.Storage;
 
 public class Controller {
-	private Storage storage;
-	private static Controller controller;
+    private Storage storage;
+    private static Controller controller;
 
-	private Controller() {
-		storage = new Storage();
-	}
+    private Controller() {
+        storage = new Storage();
+    }
 
-	public static Controller getController() {
-		if (controller == null) {
-			controller = new Controller();
-		}
-		return controller;
-	}
+    public static Controller getController() {
+        if (controller == null) {
+            controller = new Controller();
+        }
+        return controller;
+    }
 
-	public static Controller getTestController() {
-		return new Controller();
-	}
+    public static Controller getTestController() {
+        return new Controller();
+    }
 
 	/**
 	 * Hvis startDato er efter slutDato kastes en IllegalArgumentException og
@@ -89,15 +85,24 @@ public class Controller {
 		return dagligSkaev;
 	}
 
-	/**
-	 * En dato for hvornår ordinationen anvendes tilføjes ordinationen. Hvis
-	 * datoen ikke er indenfor ordinationens gyldighedsperiode kastes en
-	 * IllegalArgumentException
-	 * Pre: ordination og dato er ikke null
-	 */
-	public void ordinationPNAnvendt(PN ordination, LocalDate dato) {
-		// TODO
-	}
+    /**
+     * En dato for hvornår ordinationen anvendes tilføjes ordinationen. Hvis
+     * datoen ikke er indenfor ordinationens gyldighedsperiode kastes en
+     * IllegalArgumentException
+     * Pre: ordination og dato er ikke null
+     */
+    public void ordinationPNAnvendt(PN ordination, LocalDate dato) {
+        if (ordination == null) {
+            throw new IllegalArgumentException("Ordinationen findes ikke");
+        }
+        if (dato == null) {
+            throw new IllegalArgumentException("Dato er null");
+        }
+        boolean givet = ordination.givDosis(dato);
+        if (!givet) {
+            throw new IllegalArgumentException("Datoen er ikke inde i perioden for ordinationen");
+        }
+    }
 
     /**
      * Den anbefalede dosis for den pågældende patient (der skal tages hensyn
@@ -148,86 +153,86 @@ public class Controller {
     }
 
 
-	public List<Patient> getAllPatienter() {
-		return storage.getAllPatienter();
-	}
+    public List<Patient> getAllPatienter() {
+        return storage.getAllPatienter();
+    }
 
-	public List<Laegemiddel> getAllLaegemidler() {
-		return storage.getAllLaegemidler();
-	}
+    public List<Laegemiddel> getAllLaegemidler() {
+        return storage.getAllLaegemidler();
+    }
 
-	/**
-	 * Metode der kan bruges til at checke at en startDato ligger før en
-	 * slutDato.
-	 *
-	 * @return true hvis startDato er før slutDato, false ellers.
-	 */
-	private boolean checkStartFoerSlut(LocalDate startDato, LocalDate slutDato) {
-		boolean result = true;
-		if (slutDato.compareTo(startDato) < 0) {
-			result = false;
-		}
-		return result;
-	}
+    /**
+     * Metode der kan bruges til at checke at en startDato ligger før en
+     * slutDato.
+     *
+     * @return true hvis startDato er før slutDato, false ellers.
+     */
+    private boolean checkStartFoerSlut(LocalDate startDato, LocalDate slutDato) {
+        boolean result = true;
+        if (slutDato.compareTo(startDato) < 0) {
+            result = false;
+        }
+        return result;
+    }
 
-	public Patient opretPatient(String cpr, String navn, double vaegt) {
-		Patient p = new Patient(cpr, navn, vaegt);
-		storage.addPatient(p);
-		return p;
-	}
+    public Patient opretPatient(String cpr, String navn, double vaegt) {
+        Patient p = new Patient(cpr, navn, vaegt);
+        storage.addPatient(p);
+        return p;
+    }
 
-	public Laegemiddel opretLaegemiddel(String navn,
-			double enhedPrKgPrDoegnLet, double enhedPrKgPrDoegnNormal,
-			double enhedPrKgPrDoegnTung, String enhed) {
-		Laegemiddel lm = new Laegemiddel(navn, enhedPrKgPrDoegnLet,
-				enhedPrKgPrDoegnNormal, enhedPrKgPrDoegnTung, enhed);
-		storage.addLaegemiddel(lm);
-		return lm;
-	}
+    public Laegemiddel opretLaegemiddel(String navn,
+                                        double enhedPrKgPrDoegnLet, double enhedPrKgPrDoegnNormal,
+                                        double enhedPrKgPrDoegnTung, String enhed) {
+        Laegemiddel lm = new Laegemiddel(navn, enhedPrKgPrDoegnLet,
+                enhedPrKgPrDoegnNormal, enhedPrKgPrDoegnTung, enhed);
+        storage.addLaegemiddel(lm);
+        return lm;
+    }
 
-	public void createSomeObjects() {
-		this.opretPatient("121256-0512", "Jane Jensen", 63.4);
-		this.opretPatient("070985-1153", "Finn Madsen", 83.2);
-		this.opretPatient("050972-1233", "Hans Jørgensen", 89.4);
-		this.opretPatient("011064-1522", "Ulla Nielsen", 59.9);
-		this.opretPatient("090149-2529", "Ib Hansen", 87.7);
+    public void createSomeObjects() {
+        this.opretPatient("121256-0512", "Jane Jensen", 63.4);
+        this.opretPatient("070985-1153", "Finn Madsen", 83.2);
+        this.opretPatient("050972-1233", "Hans Jørgensen", 89.4);
+        this.opretPatient("011064-1522", "Ulla Nielsen", 59.9);
+        this.opretPatient("090149-2529", "Ib Hansen", 87.7);
 
-		this.opretLaegemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
-		this.opretLaegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
-		this.opretLaegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
-		this.opretLaegemiddel("Methotrexat", 0.01, 0.015, 0.02, "Styk");
+        this.opretLaegemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
+        this.opretLaegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        this.opretLaegemiddel("Fucidin", 0.025, 0.025, 0.025, "Styk");
+        this.opretLaegemiddel("Methotrexat", 0.01, 0.015, 0.02, "Styk");
 
-		this.opretPNOrdination(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 12),
-				storage.getAllPatienter().get(0), storage.getAllLaegemidler()
-						.get(1),
-				123);
+        this.opretPNOrdination(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 12),
+                storage.getAllPatienter().get(0), storage.getAllLaegemidler()
+                        .get(1),
+                123);
 
-		this.opretPNOrdination(LocalDate.of(2021, 2, 12), LocalDate.of(2021, 2, 14),
-				storage.getAllPatienter().get(0), storage.getAllLaegemidler()
-						.get(0),
-				3);
+        this.opretPNOrdination(LocalDate.of(2021, 2, 12), LocalDate.of(2021, 2, 14),
+                storage.getAllPatienter().get(0), storage.getAllLaegemidler()
+                        .get(0),
+                3);
 
-		this.opretPNOrdination(LocalDate.of(2021, 1, 20), LocalDate.of(2021, 1, 25),
-				storage.getAllPatienter().get(3), storage.getAllLaegemidler()
-						.get(2),
-				5);
+        this.opretPNOrdination(LocalDate.of(2021, 1, 20), LocalDate.of(2021, 1, 25),
+                storage.getAllPatienter().get(3), storage.getAllLaegemidler()
+                        .get(2),
+                5);
 
-		this.opretPNOrdination(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 12),
-				storage.getAllPatienter().get(0), storage.getAllLaegemidler()
-						.get(1),
-				123);
+        this.opretPNOrdination(LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 12),
+                storage.getAllPatienter().get(0), storage.getAllLaegemidler()
+                        .get(1),
+                123);
 
-		this.opretDagligFastOrdination(LocalDate.of(2021, 1, 10),
-				LocalDate.of(2021, 1, 12), storage.getAllPatienter().get(1),
-				storage.getAllLaegemidler().get(1), 2, 0, 1, 0);
+        this.opretDagligFastOrdination(LocalDate.of(2021, 1, 10),
+                LocalDate.of(2021, 1, 12), storage.getAllPatienter().get(1),
+                storage.getAllLaegemidler().get(1), 2, 0, 1, 0);
 
-		LocalTime[] kl = { LocalTime.of(12, 0), LocalTime.of(12, 40),
-				LocalTime.of(16, 0), LocalTime.of(18, 45) };
-		double[] an = { 0.5, 1, 2.5, 3 };
+        LocalTime[] kl = {LocalTime.of(12, 0), LocalTime.of(12, 40),
+                LocalTime.of(16, 0), LocalTime.of(18, 45)};
+        double[] an = {0.5, 1, 2.5, 3};
 
-		this.opretDagligSkaevOrdination(LocalDate.of(2021, 1, 23),
-				LocalDate.of(2021, 1, 24), storage.getAllPatienter().get(1),
-				storage.getAllLaegemidler().get(2), kl, an);
-	}
+        this.opretDagligSkaevOrdination(LocalDate.of(2021, 1, 23),
+                LocalDate.of(2021, 1, 24), storage.getAllPatienter().get(1),
+                storage.getAllLaegemidler().get(2), kl, an);
+    }
 
 }
